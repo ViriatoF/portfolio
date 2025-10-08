ba8a5bf9e26f0b6e2a3835ac7984712f8619fffa
ViriatoF <viriato.ferreira44@gmail.com>
2025-10-08 11:22:55 +0200

    Correcting few mistakes spelling in about section...again...

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -7,7 +7,7 @@ export default function About() {
     <Section id="about" title="À propos" className={`${styles.about}`}>
       <div>
         <p>Après de multiples expériences me permettant de gagner en adaptabilité, en aisance relationnelle, en maturité et en organisation, j'ai décidé, à 42 ans, de me tourner
-          vers un secteur qui m'attire depuis mon plus jeune âge:
+          vers un secteur qui m'attire depuis mon plus jeune âge :
         </p>
       </div>
       <div>
@@ -15,22 +15,22 @@ export default function About() {
       </div>
       <div>
         <p>Etant salarié, j'ai donc fais une demande de transition professionnelle pour passer le titre professionnel Développeur Web et Web Mobile (diplôme de niveau 5 équivalent à un Bac + 2)
-          à la <Link href={"https://www.wildcodeschool.com/"}>Wild Code School</Link> pour reprendre tout depuis le début et me spécialiser sur ReactJs.</p>
+          à la <a href="https://www.wildcodeschool.com/" target="_blank">Wild Code School</a> pour reprendre tout depuis le début et me spécialiser sur ReactJs.</p>
       </div>
       <div>
-        <p>Mais pourquoi donc s'arrêter là alors que l'on peut encore s'améliorer?</p>
+        <p>Mais pourquoi donc s'arrêter là alors que l'on peut encore s'améliorer ?</p>
       </div>
       <div>
         <p>Aujourd'hui, je me lance un nouveau défi (oui oui, un de plus) :</p>
-        <p>Le Master Lead Developer Front-End à l'<Link href={"https://www.ecv.fr/digital/mastere-developpement-web/"}>ECV</Link></p>
+        <p>Le Master Lead Developer Front-End à l'<a href="https://www.ecv.fr/digital/mastere-developpement-web/" target="_blank">ECV</a></p>
       </div>
       <ul className={styles.list}>
-        <li>Quand suis-je disponible?  <span className="reveal"> ASAP</span></li>
+        <li>Quand suis-je disponible ?  <span className="reveal"> ASAP</span></li>
         <li>Ce que j'aimerais faire ? </li>
         <li>Développement Front-End (ou Back-end).</li>
         <li>ReactJS, NextJS, Wordpress ou tout autre CMS.</li>
         <li>Travailler l'accessibilité, l'ergonomie, le SEO...</li>
-        <li>Participer à de petits projets, de grands projets... de petits projets qui deviendront de grands projets</li>
+        <li>Participer à de petits projets, de grands projets... de petits projets qui deviendront de grands projets.</li>
       </ul>
     </Section>
   );
