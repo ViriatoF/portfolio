e1895e96d5747b420695823df6db3d42cb7e0193
Viriato <viriato.ferreira44@gmail.com>
2026-04-28 19:53:26 +0200

    Update next, add prettier and clean footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,7 +2,6 @@ import { FaRegCopyright } from 'react-icons/fa6';
 import styles from '../footer/footer.module.css';
 
 export default function Footer() {
-  const year = new Date().getFullYear();
 
   return <></>;
 }
